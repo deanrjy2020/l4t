cbde2d651b7a8277a3fbdff4b38f1bad8ab965af
Jiayuan Ren <renjiayuan1314@gmail.com>
2018-06-21 01:30:50 +0000

    add mount host cmd

--- setupL4T.sh
+++ setupL4T.sh
@@ -1,32 +1,66 @@
 #!/bin/bash
 
-# 1, setup the display
-# do it manually.
+function first_time_setup () 
+{
+    echo "1, setup the display, do it manually..."
 
-# 2, allow root login
-echo "add the \'PermitRootLogin yes\' in the file..."
-sudo gedit /etc/ssh/sshd_config
-# #PermitRootLogin prohibit-password
-# PermitRootLogin yes
+    echo "2, allow root login. add the 'PermitRootLogin yes' in the file..."
+    sudo gedit /etc/ssh/sshd_config
+    # #PermitRootLogin prohibit-password
+    # PermitRootLogin yes
 
-# 3, setup the psswd of the root, like: r, and restart
-echo "setup the psswd of the root, like: r..."
-sudo passwd root
-systemctl restart sshd
+    echo "3, setup the psswd of the root, like: 'r'..."
+    sudo passwd root
+    systemctl restart sshd
 
-# 4, setup ip address
-echo "setup ip address..."
-sudo dhclient
+    echo "4, setup ip address..."
+    sudo dhclient
 
-# 5, save the driver version and backup the original lib
-echo "check the dirver version..."
-(cd /usr/lib/aarch64-linux-gnu/tegra ; ls -l libnvidia-* )
-read -p "input the driver version: " L4T_DRIVER_VERSION
-echo $L4T_DRIVER_VERSION > /home/nvidia/driver_version.txt
-echo "dirver version saved..."
+    echo "5, save the driver version and backup the original lib..."
+    echo "5.1, check the dirver version..."
+    (cd /usr/lib/aarch64-linux-gnu/tegra ; ls -l libnvidia-* )
+    read -p "5.2, input the driver version: " L4T_DRIVER_VERSION
+    echo $L4T_DRIVER_VERSION > /home/nvidia/driver_version.txt
+    echo "5.3, dirver version saved..."
 
-echo "backup the dirver version..."
-mkdir /home/nvidia/driver_backup
-sudo cp /usr/lib/aarch64-linux-gnu/tegra/libnvidia-* /home/nvidia/driver_backup/.
+    echo "5.4, backup the dirver version..."
+    mkdir /home/nvidia/driver_backup
+    sudo cp /usr/lib/aarch64-linux-gnu/tegra/libnvidia-* /home/nvidia/driver_backup/.
 
-echo "setup done."
+    echo "first time setup done."
+}
+
+function mount_host ()
+{
+    echo "mounting host..."
+    sudo dhclient # access the internet
+    sudo apt update
+    sudo apt install git sshfs -y
+    sudo chmod a+r /etc/fuse.conf
+    echo "uncomment the user_allow_other"
+    sudo gedit /etc/fuse.conf
+
+    mkdir mount
+
+    #read -p "input the IP and code path"
+    sshfs -o allow_other,idmap=user,reconnect,workaround=nodelaysrv jiayuanr@172.17.173.154:/home/jiayuanr/code/deva_L4T mount
+
+    echo "mounting host done..."
+}
+
+read -p "first time setting up after flashing? 0 or 1" FIRST_TIME
+read -p "mount the host source code? 0 or 1" MOUNT_HOST
+
+if [ $FIRST_TIME == "1"]
+then
+    first_time_setup
+else
+    echo "skip first time setting up., "
+fi
+
+if [ $MOUNT_HOST == "1"]
+then
+    mount_host
+else
+    echo "skip mountng host., "
+fi
