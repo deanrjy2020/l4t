0e49e3141a44b46837100b7abb0d9ceb3071a389
Jiayuan Ren <jiayuanr@nvidia.com>
2018-09-14 01:00:38 +0000

    everything in one file
    remove the backupL4TBeforeFlash.sh file
    everything is in the setupL4T.sh file.
    using the auto sourcing.

--- setupL4T.sh
+++ setupL4T.sh
@@ -9,26 +9,34 @@ function ECHO_G () {
 	echo -e ${GREEN}$*${NC}
 }
 
-function first_time_setup () 
-{
+#============================================================================
+export HOST_IP=172.17.173.154
+export HOST_NAME=jiayuanr
+export CODE_PATH=/home/jiayuanr/code/deva_L4T
+
+# need this after flashing the device.
+function dean_setup_l4t () {
 	ECHO_G "1, setup the display, do it manually..."
 
 	ECHO_G "2, allow root login. add the 'PermitRootLogin yes' in the file..."
-	sudo gedit /etc/ssh/sshd_config
+	#sudo gedit /etc/ssh/sshd_config
     # #PermitRootLogin prohibit-password
     # PermitRootLogin yes
+	echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config
 
     ECHO_G "3, setup the psswd of the root, like: 'r'..."
     sudo passwd root
     systemctl restart sshd
 
-    ECHO_G "4, setup ip address..."
-    sudo dhclient
+    ECHO_G "4, install something..."
+	sudo apt update    
+	sudo dhclient # access the internet
+    sudo apt install sshfs -y # for mount the code.
 
     ECHO_G "5, save the driver version and backup the original lib..."
     ECHO_G "5.1, check the dirver version..."
     (cd /usr/lib/aarch64-linux-gnu/tegra ; ls -l libnvidia-* )
-    read -p "5.2, input the driver version, e.g. 415.00: " L4T_DRIVER_VERSION
+    read -p "5.2, input the driver version, e.g. 418.00: " L4T_DRIVER_VERSION
     echo $L4T_DRIVER_VERSION > ~/driver_version.txt
     ECHO_G "5.3, dirver version saved..."
 
@@ -36,34 +44,38 @@ function first_time_setup ()
     mkdir ~/driver_backup
     sudo cp /usr/lib/aarch64-linux-gnu/tegra/libnvidia-* ~/driver_backup/.
 
+	# this is for ssh. scp still needs the passwd.
+	ECHO_G "ssh key setup..."
+	ssh-keygen
+	# on your host, you also need to do "chmod 700 ~/.ssh/authorized_keys"
+	cat ~/.ssh/id_rsa.pub | ssh ${HOST_NAME}@${HOST_IP} 'cat >> .ssh/authorized_keys'
+
     ECHO_G "first time setup done."
 }
 
-function mount_host ()
-{
+function dean_backup_l4t () {
+	# qt creator info
+	scp /home/nvidia/.config/QtProject/qtcreator/*.qws ${HOST_NAME}@${HOST_IP}:/home/jiayuanr/bin/l4t_device_backup/
+}
+
+# need this after reboot. seems there is an issue with auto mount.
+function dean_mount_host () {
     ECHO_G "mounting host..."
 
-    # the user name will always be 'jiayuanr'
-    read -p "input the IP of the host: " CODE_HOST_IP
     sudo chmod a+r /etc/fuse.conf
-    ECHO_G "uncomment the 'user_allow_other' "
-    sudo gedit /etc/fuse.conf
-
-    ECHO_G "after this point, all you need to do is waiting."
-    sudo dhclient # access the internet
-    sudo apt update
-    sudo apt install git sshfs -y
+    ECHO_G "adding the 'user_allow_other' to /etc/fuse.conf"
+    #sudo gedit /etc/fuse.conf
+	echo 'user_allow_other' | sudo tee -a /etc/fuse.conf
 
     # the local folder name.
     mkdir ~/mount
     #sshfs -o allow_other,idmap=user,reconnect,workaround=nodelaysrv jiayuanr@172.17.173.154:/home/jiayuanr/code/deva_L4T ~/mount
-    sshfs -o allow_other,idmap=user,reconnect,workaround=nodelaysrv jiayuanr@${CODE_HOST_IP}:${CODE_PATH} ~/mount
+    sshfs -o allow_other,idmap=user,reconnect,workaround=nodelaysrv ${HOST_NAME}@${HOST_IP}:${CODE_PATH} ~/mount
 
     ECHO_G "mounting host done..."
 }
 
-function install_qtcreator ()
-{
+function dean_install_qtcreator () {
 	ECHO_G "installing qtcreator..."
     # we need this script for qtcreator debug.
 	echo set substitute-path ${CODE_PATH} /home/nvidia/mount > ~/qt.sh
@@ -77,40 +89,17 @@ function install_qtcreator ()
     sleep 1 s
 
     ECHO_G "scp the qt creator qws files to device..."
-    scp jiayuanr@${CODE_HOST_IP}:/home/jiayuanr/bin/l4t_device_backup/*.qws /home/nvidia/.config/QtProject/qtcreator/
+	# I don't want to write my host passwd here. type it.
+    scp ${HOST_NAME}@${HOST_IP}:/home/jiayuanr/bin/l4t_device_backup/*.qws /home/nvidia/.config/QtProject/qtcreator/
 
     ECHO_G "installing qtcreator done..."
 }
 
-read -p "first time setting up after flashing? 0 or 1: " FIRST_TIME
-read -p "mount the host source code? 0 or 1: " MOUNT_HOST
-read -p "install qt creator? 0 or 1: " INSTALL_QT
-read -p "input the code path, e.g. /home/jiayuanr/code/deva_L4T : " CODE_PATH
-
-if [ $FIRST_TIME == "1" ]
-then
-	# need this after flashing the device.
-    first_time_setup
-else
-    ECHO_G "skip first time setting up."
-fi
-
-if [ $MOUNT_HOST == "1" ]
-then
-	# need this after reboot.
-    mount_host
-else
-    ECHO_G "skip mountng host."
-fi
-
-if [ $INSTALL_QT == "1" ]
-then
-	# if you mount the source code, probably need qtcreator.
-	# takes 10~20 min
-	install_qtcreator
-else
-    ECHO_G "skip installing qt creator."
-fi
+# after flashing the device.
+# source l4t/setupL4T.sh ; dean_source
+function dean_source () {
+	echo 'source /home/nvidia/l4t/setupL4T.sh' | sudo tee -a /home/nvidia/.bashrc
+}
 
 
 
