0fea7e3413b6e1d5dee3c7b36e2e78197ac8b415
Jiayuan Ren <renjiayuan1314@gmail.com>
2019-09-06 11:31:51 -0700

    dean_weston_prepare

--- setupL4T.sh
+++ setupL4T.sh
@@ -118,15 +118,28 @@ function dean_install_qtcreator () {
     ECHO_G "installing qtcreator done..."
 }
 
-function dean_switch_to_weston () {
+function dean_weston_prepare () {
     # Ctrl + Alt + F3 (or ssh or something else), and then run this.
-    sudo pkill -9 Xorg
     sudo service gdm3 stop
+    sudo pkill -9 Xorg
+
     sudo modprobe tegra-udrm modeset=1 # this is for weston 6
     sudo mkdir /tmp/xdg
     sudo chmod 777 /tmp/xdg
-    sudo XDG_RUNTIME_DIR=/tmp/xdg weston --use-egldevice --tty=2
-    sudo XDG_RUNTIME_DIR=/tmp/xdg /usr/bin/weston-simple-egl
+
+    # normal run
+
+    # sudo XDG_RUNTIME_DIR=/tmp/xdg weston --use-egldevice --tty=2
+
+    # sudo XDG_RUNTIME_DIR=/tmp/xdg weston --use-egldevice --tty=2 --modules=/usr/lib/aarch64-linux-gnu/tegra/weston/weston-test.so
+
+    #--log=/home/nvidia/dean/log.txt
+    #--socket=test-text
+
+    # gdb debug
+    # sudo XDG_RUNTIME_DIR=/tmp/xdg gdbserver :5039 weston --use-egldevice --tty=2 --modules=/usr/lib/aarch64-linux-gnu/tegra/weston/weston-test.so
+
+    # sudo XDG_RUNTIME_DIR=/tmp/xdg /usr/bin/weston-simple-egl
 }
 
 # after flashing the device.
